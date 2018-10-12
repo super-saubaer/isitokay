a5406bb82f7f664c9350a229f30d0f253a0e25e0
super-saubaer <sebastian.t.siepe@googlemail.com>
2018-10-12 21:19:44 +0200

    Material Design Icons and Typewriter animation added

--- isitokay/client/question.js
+++ isitokay/client/question.js
@@ -3,6 +3,44 @@
  */
 
 $(document).ready(function () {
+    // Typewriter
+    let counter1 = 0;
+    let title1 = 'You have a weird question? ';
+    let counter2 = 0;
+    let title2 = 'Good. Go ahead & answer it! ';
+    let speed = 100; /* The speed/duration of the effect in milliseconds */
+
+    typeTitel1();
+
+    /**
+     * typeTytle Function
+     */
+    function typeTitel1() {
+        if (counter1 < title1.length) {
+            document.getElementById('title1').innerHTML += title1.charAt(counter1);
+            counter1++;
+            setTimeout(typeTitel1, speed);
+        } else {
+            console.log('read here');
+            typeTitel2();
+        }
+    }
+
+    /**
+     * typeTytle Function
+     */
+    function typeTitel2() {
+        if (counter2 < title2.length) {
+            document.getElementById('title2').innerHTML += title2.charAt(counter2);
+            counter2++;
+            setTimeout(typeTitel2, speed);
+        } else {
+            $('.container').css('opacity', 1);
+        }
+    }
+
+    
+
     // Simple json data model
     let dataModel = {
         question: '',
@@ -31,7 +69,7 @@ $(document).ready(function () {
             let url = window.location + '?generate';
             console.log(url);
 
-            $.post(url, JSON.stringify(dataModel), function(result) {
+            $.post(url, JSON.stringify(dataModel), function (result) {
                 console.log(decodeURIComponent(result));
             });
         }
